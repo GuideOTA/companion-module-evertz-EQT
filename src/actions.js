8147d4c85d4763c040a2e1006ebbe2f38c09409c
GuideOTA <cole.bewley@kjrh.com>
2025-04-16 10:47:49 -0500

    Update actions.js

--- src/actions.js
+++ src/actions.js
@@ -54,7 +54,76 @@ module.exports = {
 				self.sendCommand(command)
 			},
 		}
-
+		actions[select_route] = {
+			name: 'Route Crosspoint',
+			description: 'Select a Route',
+				options: [
+					{
+						type: 'number',
+						label: 'source',
+						id: 'src',
+						default: 1,
+						min: 1,
+						max: max_sources,
+						range: false,
+						isVisible: (options) => options.useVariable === false || options.useVariable === undefined
+					},
+					{
+						type: 'textinput',
+						label: 'source',
+						id: 'srcvar',
+						default: '',
+						useVariables: true,
+						isVisible: (options) => options.useVariable === true
+					},
+							{
+								type: 'checkbox',
+								label: 'Use Variable',
+								id: 'useVariable',
+								default: false,
+							},
+					{
+						type: 'number',
+						label: 'destination',
+						id: 'dst',
+						default: 1,
+						min: 1,
+						max: max_destinations,
+						range: false,
+						isVisible: (options) => options.useVariable === false || options.useVariable === undefined
+					},
+					{
+						type: 'textinput',
+						label: 'destination',
+						id: 'dstvar',
+						default: '',
+						useVariables: true,
+						isVisible: (options) => options.useVariable === true
+					},
+							{
+								type: 'checkbox',
+								label: 'Use Variable',
+								id: 'useVariable',
+								default: false,
+							},
+					{
+					type: 'textinput',
+					id: 'levels',
+					label: 'Levels',
+					width: 6,
+					default: 'V',
+					useVariables: true,
+					},
+							
+			],
+				callback: async function (action) {
+					let options = action.options
+					let levels = await self.parseVariablesInString(options.levels)
+					if (!options.useVariable) command = `.S${levels}${options.dst},${options.src}`
+					else command = `.S${levels}${options.dstvar},${options.srcvar}`
+					await self.sendCommand(command)
+			},
+		}
 		actions['set_xpt'] = {
 			name: 'Route Source to Destination',
 			description: 'Route a source to a destination',
